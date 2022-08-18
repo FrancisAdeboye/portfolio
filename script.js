db46de67210a5f5b8e3351125e6bd859c746bbcc
FrancisAdeboye <105540438+FrancisAdeboye@users.noreply.github.com>
2022-08-18 16:10:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+showSlides();
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("swiper-slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
