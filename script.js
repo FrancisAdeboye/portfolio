1a0d91cbe5ca447ff205868192410a5c53e0646f
Francis Adeboye <francisadeboye24@gmail.com>
2022-08-19 22:53:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+var typed = new Typed(".auto-input", {
+  strings: ["", "I'm a Web"],
+  typeSpeed: 300,
+  backSpeed: 200,
+  loop: true,
+})
+var autoTyped = new Typed(".auto-input-2", {
+  strings: ["", "Frontend Web Developer"],
+  typeSpeed: 300,
+  backSpeed: 100,
+  loop: true,
+})
+var autoTyped2 = new Typed(".auto-input-3", {
+  strings: ["", "I will help you reach your goal by providing help building your new platform in a timely manner."],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+})
+
 let slideIndex = 0;
 showSlides();
-
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
